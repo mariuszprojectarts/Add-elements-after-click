6320e5484eb09642fb39a56a7e6fb7507ee2b73c
Mariusz Szleszynski <mariusz.szleszynski@outlook.com>
2020-10-12 22:29:33 +0200

    Add fn addElement

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 const btn = document.querySelector('button');
 let number = 1;
+let activeNumber = 1;
 
 const addElement = function () {
     const div = document.createElement('div');
     div.textContent = number
-    document.body.appendChild(div);
 
-    if (Number.isInteger(number / 5)) {
-        div.classList.add('circle')
+    if (number % 5 == 0) {
+        div.classList.add('circle');
     }
+
+    // if (Number.isInteger(number / 5)) {
+    //     div.classList.add('circle')
+    // }
+
+    // if (activeNumber == 5) {
+    //     activeNumber = 0;
+    //     div.classList.add('circle');
+    // }
+
+    document.body.appendChild(div);
     number++;
+    activeNumber++;
     // console.log(div);
 }
 
